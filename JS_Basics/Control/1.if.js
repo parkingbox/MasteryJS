2dfcd9ecf7dee4b2d71e01d09faa281aaacfc37e
밖쓰 <101634719+parkingbox@users.noreply.github.com>
2022-04-13 19:56:55 +0900

    JS_Basics Control

--- JS_Basics/Control/1.if.js
+++ JS_Basics/Control/1.if.js
@@ -1,1 +1,17 @@
+// 조건문 Conditional Statement
+// if(조건) {}
+// if(조건) {} else {}
+// if(조건1) {} else if (조건2) else {}
+let fruit = 'orange';
+if (fruit === 'apple'){
+  console.log('🍎');
+} else if(fruit === 'orange'){
+  console.log('🍊');  
+  } else {
+  console.log('😀');
+}
 
+// true or false
+if (2 > 1) {
+  console.log('출력되면 안됨!');
+}
