e24070a77f2f61f2d4b6e3098510ba3247fb80db
밖쓰 <101634719+parkingbox@users.noreply.github.com>
2022-04-22 19:17:33 +0900

    JS_Basics Iterable

---
+++ JS_Basics/Iterable/3.iterable-quiz-function.js
@@ -0,0 +1,24 @@
+// [Symbol.iterator](): IterableIterator<T>;
+// 0 부터 10이하 까지 숫자의 2배를 순회하는 이터레이터(반복자) 만들기!
+function makeIterable(initialValue, maxValue, callback) {
+  return {
+  [Symbol.iterator]:() => {
+    const max = maxValue;
+    let num = initialValue;
+    return {
+      next() {
+        return {value: callback(num++), done: num > max};
+      },
+    };
+  },
+};
+}
+const multiple = makeIterable(0, 20, (num) => num *2);
+for(const num of multiple){
+  console.log(num);
+}
+
+  const single = makeIterable(0, 20, (num) => num);
+  for (const num of single){
+    console.log(num);
+  }
