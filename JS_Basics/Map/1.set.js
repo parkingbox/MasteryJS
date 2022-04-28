e3efaad1b74f3653ec9f2a3610506c287239b65e
밖쓰 <101634719+parkingbox@users.noreply.github.com>
2022-04-28 21:32:40 +0900

    JS_Basics Map

--- JS_Basics/Map/1.set.js
+++ JS_Basics/Map/1.set.js
@@ -1,1 +1,47 @@
+// Set 
+const set = new Set([1, 2, 3]);
+console.log(set);
 
+console.log(set.size);
+
+// 존재하는지 확인
+console.log(set.has(2));
+console.log(set.has(6));
+
+// 순회
+set.forEach(item => console.log(item));
+for(const value of set.values()) {
+  console.log(value);
+}
+
+// 추가 add   *중복불가
+set.add(6);
+console.log(set);
+set.add(6);
+console.log(set);
+
+// 삭제
+set.delete(6);
+console.log(set);
+
+// 전부 삭제
+set.clear();
+console.log(set);
+
+// 오브젝트 세트
+const obj1 = {name: '🍓', price : 8};
+const obj2 = {name: '🍎', price : 5};
+const objs = new Set([obj1, obj2]);
+console.log(objs);
+
+// 퀴즈
+obj1.price = 10;
+objs.add(obj1);
+console.log(objs);
+
+// 퀴즈
+const obj3 = {name: '🍎', price : 5};
+objs.add(obj3);
+console.log(objs);
+obj3.price = 8;
+console.log(objs);
