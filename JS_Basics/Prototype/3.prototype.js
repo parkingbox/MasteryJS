40b631cc1dd435e8b9da149e9dc68523329417b9
밖쓰 <101634719+parkingbox@users.noreply.github.com>
2022-06-09 02:37:28 +0900

    JS_Basics Prototype

---
+++ JS_Basics/Prototype/3.prototype.js
@@ -0,0 +1,39 @@
+
+  function Dog(name, emoji) {
+    this.name = name;
+    this.emoji = emoji;
+
+  // 인스턴스 레벨의 함수  
+  /* this.printName = () => {
+    console.log(`${this.name} ${this.emoji}`);
+  }; */
+}
+
+// 프로토타입 레벨의 함수
+Dog.prototype.printName = function () {
+  console.log(console.log(`${this.name} ${this.emoji}`))
+};
+
+const dog1 = new Dog('뭉치', '🐶');
+const dog2 = new Dog('뭉치', '🐗');
+console.log(dog1, dog2);
+dog1.printName();
+dog2.printName(); 
+
+// 프로토타입의 레벨의 함수
+
+//오버라이딩
+// 인스턴스 레벨에(자식) 동일한 이름으로 함수를 재정의 하면 (오버라이딩 하면)
+// 프로토타입 레벨의(부모) 함수의 프로퍼티는 가려진다 (섀도잉 됨)
+dog1.printName = function () {
+  console.log('안녕!!');
+};
+dog1. printName();
+
+//정적 레벨
+Dog.hello = () => {
+  console.log('Hello!');
+};
+Dog.hello();
+Dog.Max_AGE = 20;
+
