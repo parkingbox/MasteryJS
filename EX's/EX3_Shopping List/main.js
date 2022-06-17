924c093b5708908e6ba32d5e9e3a0e505277624e
밖쓰 <101634719+parkingbox@users.noreply.github.com>
2022-06-17 21:21:12 +0900

    DOM example

--- EX's/EX3_Shopping List/main.js
+++ EX's/EX3_Shopping List/main.js
@@ -1,1 +1,11 @@
+function addList() {
+  const addValue = document.getElementById('addValue').value;
 
+  const li = document.createElement("li");
+  li.setAttribute('id',addValue);
+
+  const textNode = document.createTextNode(addValue);
+  li.appendChild(textNode);
+
+  document.getElementById()
+}
