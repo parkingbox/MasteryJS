a11cda3ebabb4c237a2b5a489308d4095623728f
밖쓰 <101634719+parkingbox@users.noreply.github.com>
2022-06-19 14:57:01 +0900

    DOM example

--- EX's/EX3_Shopping List/main.js
+++ EX's/EX3_Shopping List/main.js
@@ -1,11 +1,4 @@
-function addList() {
-  const addValue = document.getElementById('addValue').value;
-
-  const li = document.createElement("li");
-  li.setAttribute('id',addValue);
-
-  const textNode = document.createTextNode(addValue);
-  li.appendChild(textNode);
-
-  document.getElementById()
+function printName()  {
+  const name = document.getElementById('list__input__write').value;
+  document.getElementById("result").innerText = name;
 }
